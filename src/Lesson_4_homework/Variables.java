0a842032645f0b0d487ccf0e63fe9b43b339ea53
tekwilljunior <junomivo@gmail.com>
2025-03-06 11:14:06 +0200

    Homework++

---
+++ src/Lesson_4_homework/Variables.java
@@ -0,0 +1,20 @@
+package Lesson_4_homework;
+
+public class Variables {
+    //instance variables
+    long Current_World_Population = 7811080815l;
+    int Cars_Produced_This_Year = 55235358;
+    char First_Letter_In_Alphabet = 'A';
+    boolean Java_Is_Amazing = true;
+    double Pi = 3.14159;
+
+
+    //Constructor_1
+    public Variables() {
+        System.out.println("Current World Population: " + Current_World_Population);
+        System.out.println("Cars Produced This Year: " + Cars_Produced_This_Year);
+        System.out.println("First Letter in Alphabet: " + First_Letter_In_Alphabet);
+        System.out.println("Is Java Amazing? " + Java_Is_Amazing);
+        System.out.println("Value of Pi:" + Pi);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import greetings.append;
 import greetings_1.append_1;
 import HelloJavaWorld.greet_me;
+import Lesson_4_homework.Variables;
 class greetv {
     public static void main(String[] args) {
-        greet_me hi_from_marin = new greet_me(/*"Greetings", "suckers", "world"*/);
+        greet_me hi_from_marin = new greet_me("Greetings", "suckers", "world");
 
         System.out.println(hi_from_marin.greet + " " + hi_from_marin.whom + " " + hi_from_marin.community);
 
@@ -13,5 +14,8 @@ class greetv {
 
         append_1 availables_2 = new append_1();
         availables_2.printMessage_1();
+
+        Variables display_homework_from_package = new Variables();
+
     }
 }
